38f9522a1bbc30526635ddeecb5391307b6535fa
Vladimir Uspenskiy <vl.uspenskiy@gmail.com>
2015-03-22 03:44:46 -0700

    Removed dependency for specific object for clustering

--- src/main/scala/vu/ml/model.scala
+++ src/main/scala/vu/ml/model.scala
@@ -7,21 +7,4 @@ package vu.ml
 
 case class Id[+T](value: String)
 
-// Fields' order sets importance for classifier (most important first)
-case class Operation(brand: Option[String],
-                     mcc: Option[String],
-                     category: Option[String],
-                     isCardNotPresent: Option[Boolean],
-                     city: Option[String],
-                     country: Option[String],
-                     amount: Option[Double],
-                     currency: Option[String],
-                     group: Option[String],
-                     isPaymentGate: Option[Boolean],
-                     isRegular: Option[Boolean],
-                     time: Option[Long],
-                     timeOfDay: Option[String],
-                     isWeekend: Option[Boolean],
-                     isWorkingTime: Option[Boolean])
-
 case class Cluster[T](id: Id[Cluster[T]], objects: Array[T], typicalObject: T)
