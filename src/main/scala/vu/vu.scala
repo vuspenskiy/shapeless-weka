631d583058ccf5ebfe0cf71335923c8e7457b832
Vladimir Uspenskiy <v.uspenskiy@icloud.com>
2015-10-05 04:13:23 +0300

    Resolving package conflicts

--- src/main/scala/vu/vu.scala
+++ src/main/scala/vu/vu.scala
@@ -1,4 +1,6 @@
 
+import scala.util.Try
+
 /**
  * Generic util functions
  */
@@ -6,4 +8,8 @@
 package object vu {
 
   def make[T](p: => T)(s: T => Unit) = { val v = p; s(v); v }
+
+  def using[T <: { def close() }, R](p: => T)(s: T => R): R = { val r = s(p); Try(p.close()); r }
+
+  def fix[A,B](f: (A=>B) => (A=>B)): A=>B = f(fix(f))(_)
 }
