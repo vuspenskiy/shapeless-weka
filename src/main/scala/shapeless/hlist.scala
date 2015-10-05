4a0fb7b6f463c61faa06094d26f031354347f530
Vladimir Uspenskiy <v.uspenskiy@icloud.com>
2015-10-05 04:11:02 +0300

    Resolving package conflicts

--- src/main/scala/shapeless/hlist.scala
+++ src/main/scala/shapeless/hlist.scala
@@ -1,6 +1,7 @@
 package shapeless
 
 import shapeless.ops.hlist.{Length, Zip}
+import shapeless.extensions._
 
 /**
  * Method on `HList` to zip with range of `Nat` from zero to `HList` length minus one
@@ -8,7 +9,7 @@ import shapeless.ops.hlist.{Length, Zip}
  * @author Travis Brown
  */
 
-object hlist {
+object HListExtensions {
 
   def zipWithIndex[L <: HList, S <: Nat, R <: HList, Out <: HList](l: L)
     (implicit len: Length.Aux[L, S], range: Range.Aux[nat._0, S, R], zipper: Zip.Aux[L :: R :: HNil, Out]): Out =
